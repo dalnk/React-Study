011b8a1201035b71aecf7a8d74a8d54daeec6128
Dennis Aleynikov <daleynikov@gmail.com>
2018-02-26 12:25:59 -0800

    taking turns works

--- tic-tac/src/index.js
+++ tic-tac/src/index.js
@@ -3,28 +3,36 @@ import ReactDOM from 'react-dom'
 import './style.css'
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: null
-    }
-  }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    squares[i] = this.state.xIsNext ? 'X' : '0'
+    this.setState({squares: squares, xIsNext: !this.state.xIsNext})
+  }
+
   renderSquare(i) {
-    return <Square  value={i}/>;
+    return <Square  value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : '0');
 
     return (
       <div>
